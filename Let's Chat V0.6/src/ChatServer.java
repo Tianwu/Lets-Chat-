b3a64778eb1786547f53739ca5186e1c6990d964
Tianwu <2652017679@qq.com>
2016-10-07 11:19:17 +0800

    V0.6
    客户端不断输入一句话，服务器能收到 使用while(true) {}

---
+++ Let's Chat V0.6/src/ChatServer.java
@@ -0,0 +1,47 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
+import javax.swing.JOptionPane;
+
+public class ChatServer {
+	private ServerSocket ss = null;
+	private Socket s = null;
+	private DataInputStream dis = null;
+	
+	public static void main(String[] args) {
+		new ChatServer().connect();
+
+	}
+
+	public void connect() {
+		try {//BindException
+			ss = new ServerSocket(9600);	
+System.out.println("A Server openning...");
+			JOptionPane.showMessageDialog(null, "服务器打开", "提示", JOptionPane.INFORMATION_MESSAGE);
+		} catch (BindException be) {
+			JOptionPane.showMessageDialog(null, "请不要重复打开服务器", "警告", JOptionPane.WARNING_MESSAGE);
+System.exit(0);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			s = ss.accept();
+System.out.println("A Client Connected...");
+			while(true) {	
+				dis = new DataInputStream(s.getInputStream());
+				String str = dis.readUTF();
+System.out.println(str);
+			}
+		} catch (SocketException se) {
+System.out.println("A Client quit...");
+System.exit(0);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
