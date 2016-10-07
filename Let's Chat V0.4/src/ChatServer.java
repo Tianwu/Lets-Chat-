31ca0925e8762b1d90b27b166fc702f429d0e934
Tianwu <2652017679@qq.com>
2016-10-07 11:18:18 +0800

    V0.4
    起一个服务器端ChatServer.java，基于TCP的
    开启服务器，开启客户端，两者能连上

---
+++ Let's Chat V0.4/src/ChatServer.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javax.swing.JOptionPane;
+
+public class ChatServer {
+	private ServerSocket ss = null;
+	private Socket s = null;
+	public static void main(String[] args) {
+		new ChatServer().connect();
+
+	}
+
+	public void connect() {
+		try {//BindException
+			ss = new ServerSocket(9600);
+System.out.println("A Server openning...");
+			JOptionPane.showMessageDialog(null, "服务器已打开", "提示", JOptionPane.INFORMATION_MESSAGE);
+		} catch (BindException be) {
+			JOptionPane.showMessageDialog(null, "请不要重复打开服务器", "警告", JOptionPane.WARNING_MESSAGE);
+System.exit(0);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			s = ss.accept();
+System.out.println("A Client Connected...");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
